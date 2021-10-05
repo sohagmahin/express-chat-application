4a939e698961dc56a0f1325cbe4810630ee8e836
sohagmahin <sohagmahin@gmail.com>
2021-10-05 19:34:48 +0600

    add avatar uploader by multer:sparkles:

---
+++ middlewares/common/avatarUpload.js
@@ -0,0 +1,27 @@
+const uploader = require("../../utilities/singleUploader");
+
+function avaterUpload(req, res, next) {
+  const upload = uploader(
+    "avaters",
+    ["images/jpeg", "image/jpg", "image/png"],
+    1000000,
+    "Only .jpg, .jpeg or .png format allowed!"
+  );
+
+  //   call the middleware function
+  upload.any()(req, res, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: {
+          avatar: {
+            msg: err.message,
+          },
+        },
+      });
+    } else {
+      next();
+    }
+  });
+}
+
+module.exports = avaterUpload;
