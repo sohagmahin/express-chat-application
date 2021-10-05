382aa7ae6a0b9863860d283f8e050927070437e8
sohagmahin <sohagmahin@gmail.com>
2021-10-05 20:32:05 +0600

    write add user controller :sparkles:

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,6 +1,47 @@
+// external imports
+const bcrypt = require("bcrypt");
+
+// internal imports
+const User = require("../model/People");
+
 // get users page
 function getUsers(req, res, next) {
   res.render("users");
 }
 
-module.exports = { getUsers };
+// add user
+async function addUser(req, res, next) {
+  let newUser;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  if (req.files && req.files.length > 0) {
+    newUser = new User({
+      ...req.body,
+      avatar: req.files[0].filename,
+      password: hashedPassword,
+    });
+  } else {
+    newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
+  }
+
+  // save user or send error
+  try {
+    const result = newUser.save();
+    res.status(200).json({
+      message: "User was added successfully!",
+    });
+  } catch (err) {
+    res.status(500).json({
+      errors: {
+        common: {
+          msg: "Unknown error occured!",
+        },
+      },
+    });
+  }
+}
+
+module.exports = { getUsers, addUser };
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 // internal imports
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUser } = require("../controller/usersController");
 const avatarUpload = require("../middlewares/users/avatarUpload");
 const {
   addUserValidators,
@@ -16,6 +16,12 @@ const router = express.Router();
 router.get("/", decorateHtmlResponse("Users"), getUsers);
 
 // add user
-router.post("/", avatarUpload, addUserValidators, addUserValidationHandler);
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 module.exports = router;
