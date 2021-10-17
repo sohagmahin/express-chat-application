d98ffc1ba45fa6fc3be7188b1da8fa1ecb8bfdae
sohagmahin <sohagmahin@gmail.com>
2021-10-17 19:56:35 +0600

    add auth guard

---
+++ middlewares/common/checkLogin.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+
+const checkLogin = (req, res, next) => {
+  let cookies =
+    Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
+  if (cookies) {
+    try {
+      const token = cookies[process.env.COOKIE_NAME];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+
+      //  pass user info to response locals
+      if (res.locals.html) {
+        res.locals.loggedInUser = decoded;
+      }
+      next();
+    } catch (err) {
+      if (res.locals.html) {
+        res.redirect("/");
+      } else {
+        res.status(500).json({
+          errors: {
+            common: {
+              msg: "Authentication failure!",
+            },
+          },
+        });
+      }
+    }
+  } else {
+    if (res.locals.html) {
+      res.redirect("/");
+    } else {
+      res.status(401).json({
+        error: "Authentication failer!",
+      });
+    }
+  }
+};
+
+module.exports = {
+  checkLogin,
+};
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -13,15 +13,16 @@ const {
   addUserValidators,
   addUserValidationHandler,
 } = require("../middlewares/users/userValidators");
-
+const { checkLogin } = require("../middlewares/common/checkLogin");
 const router = express.Router();
 
 // users page
-router.get("/", decorateHtmlResponse("Users"), getUsers);
+router.get("/", decorateHtmlResponse("Users"), checkLogin, getUsers);
 
 // add user
 router.post(
   "/",
+  checkLogin,
   avatarUpload,
   addUserValidators,
   addUserValidationHandler,
